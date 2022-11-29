74d4b3cf1d519ed190b40033a690834e0c8aa07a
ryan-doucette <doucette.ry@northeastern.edu>
2022-11-28 21:19:02 -0500

    CORS error bug fix attempt 6

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ const app = express();
 
 app.use(cors());
 
+
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-
-app.use(express.json());
 
 HelloController(app);
 UserController(app);
