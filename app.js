4861fb0741c9493a673395040cbdfa15323049fc
ryan-doucette <doucette.ry@northeastern.edu>
2022-11-28 21:25:03 -0500

    CORS error bug fix attempt 8

--- app.js
+++ app.js
@@ -11,16 +11,11 @@ mongoose.connect(CONNECTION_STRING);
 import cors from 'cors';
 const app = express();
 
-
-app.use(cors());
-
+app.use(cors({
+    credentials: true
+}));
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 HelloController(app);
 UserController(app);
